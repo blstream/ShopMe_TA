3ddf836afb01437d69916882f2a51a83b38d74e3
Magdalena Jedraszak <magdalena.jedraszak@gmail.com>
2018-03-08 17:24:08 +0100

    pierwszy commit do magda

---
+++ src/test/java/Features/CucumberTests.java
@@ -0,0 +1,15 @@
+package Features;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "Features"
+        , glue = {"Steps"}
+)
+
+public class CucumberTests {
+
+}
