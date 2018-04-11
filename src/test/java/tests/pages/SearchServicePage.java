b40128ce4575cf6b5b52167c50167b3060568915
UlaLib <ulibront@gmail.com>
2018-04-11 18:55:44 +0200

    PRODPFIP-45 feature without unnecessary imports

--- src/test/java/tests/pages/SearchServicePage.java
+++ src/test/java/tests/pages/SearchServicePage.java
@@ -5,10 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static tests.Hooks.driver;
-import static tests.Hooks.wait;
 
 public class SearchServicePage {
 
