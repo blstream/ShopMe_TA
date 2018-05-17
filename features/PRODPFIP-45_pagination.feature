707a2a11761cb26c39f30f2341bcfc490b739e30
UlaLib <ulibront@gmail.com>
2018-05-17 08:12:56 +0200

    changes in pagination test

--- features/PRODPFIP-45_pagination.feature
+++ features/PRODPFIP-45_pagination.feature
@@ -1,80 +1,96 @@
-@inProgress
+@inProgres
 Feature: PRODPFIP-45 Pagination of search results
   As a user I want to navigate between search result pages so that I can see all the search results
 
-  Background:
-    Given I add 500 services with the title "test 1"
-    And I add 11 services with the title "test 2"
+    Scenario: Displaying all existing services
+      Given that there are no services added
+      And I add 54 different services
+      # | service name | category | user name | user email      | user phone | user info | base description | base price | extended description | extended price | extra description | extra price | province          | city     |
+        | test         | law      | test      | test@domain.com | 888555222  | test      | test             | 10         | test                 | 20             | test              | 30          | WesternPomeranian | Szczecin |
+      When I go to ShopMe main page
+      And I enter a searching phrase "test" into the search field
+      And I click the search button
+      Then I can go to any service from database with title "test"
 
-  Scenario Outline: Correct displaying pagination
+  Scenario: Correct displaying pagination
     When I go to ShopMe main page
-    And I enter a searching phrase "<searching_phrase>" into the search field
+    And I enter a searching phrase "test" into the search field
     And I click the search button
     Then search results are visible
-    And I am on the first page
-    And I can see 10 records
-    And I can see pagination buttons
-    And I can see that number 1 is bold
-    And I can see how many search result pages there are
+#    And I can see 10 records
+#    And I can see pagination buttons
+#    And I can see that number 1 is bold
+#    And I can see how many search result pages there are
 
-    Examples:
-      | searching_phrase |
-      | test 1           |
-      | test 2           |
-
-  Scenario Outline: Correct number of results on each page
-    Given I go to ShopMe main page
-    And I enter a searching phrase "<searching_phrase>" into the search field
-    When I click the search button
-    Then the last page contains maximum 10 results from <number_of_results>
-    And other pages contain 10 results
-
-    Examples:
-      | searching_phrase | number_of_results |
-      | test 1           | 500               |
-      | test 2           | 11                |
-
-  Scenario: Correct navigation with number-button between the pages
-    Given I go to ShopMe main page
-    When I enter a searching phrase "test 1" into the search field
-    And I click the search button
-    And search results are visible
-    And I click the second page
-    Then I am on the second page
-    And I can see that number 2 is bold
-    And previous-button is visible
-    And I can return to the first page
-
-  Scenario: Correct navigation on the last page
-    Given I go to ShopMe main page
-    When I enter a searching phrase "test 1" into the search field
-    And I click the search button
-    And search results are visible
-    And I click the last page
-    Then I am on the last page
-    And I can see the numbers of last three pages
-    And I can see the number of the first page
-    And next-button is invisible
-
-  Scenario: Correct navigation with arrow-button between the pages
-    Given I go to ShopMe main page
-    When I enter a searching phrase "test 1" into the search field
-    And I click the search button
-    And search results are visible
-    And I click next-button
-    Then I navigate to the second page
-    And previous-button is visible
-    And I can return to the first page by clicking previous-button
-
-  Scenario: Add offer and check correctness indexing
-    Given I go to ShopMe main page
-    And I enter a searching phrase "test 1" into the search field
-    And I click the search button
-    And search results are visible
-    And I navigate to the second page
-    When I add services
-      | test 1 |
-    And I return to the first page
-    Then newly added service should be displayed in the first place
-    And the last result of each page before adding service is the first result of the next page after adding service
-        
+#  Scenario Outline: Correct number of results on each page
+#    Given I add <number_of_services> different services
+#    # | service name       | category | user name | user email      | user phone | user info | base description | base price | extended description | extended price | extra description | extra price | province          | city     |
+#      | <searching_phrase> | law      | test      | test@domain.com | 888555222  | test      | test             | 10         | test                 | 20             | test              | 30          | WesternPomeranian | Szczecin |
+ #When I go to ShopMe main page
+#    And I enter a searching phrase "<searching_phrase>" into the search field
+#    And I click the search button
+#   Then Every page but the last one contain 10 results
+#    And The last page contains maximum 10 results
+#
+#    Examples:
+#      | searching_phrase | number_of_results |
+#      | firstTest           | 1               |
+#      | secondTest           | 10                |
+#
+#  Scenario: Correct navigation with number-button between the pages
+#    Given I go to ShopMe main page
+#    When I enter a searching phrase "test" into the search field
+#    And I click the search button
+#    And search results are visible
+#    And I click the second page button
+#    Then
+# I can see that number 2 is bold
+  # And I can see 10 results from the second page
+#    And previous-button is visible
+#    And I can return to the first page
+#
+#  Scenario: Correct navigation on the last page
+#    Given I go to ShopMe main page
+#    When I enter a searching phrase "test" into the search field
+#    And I click the search button
+#    And search results are visible
+#    And I click the last page button
+#  Then search results are visible
+ # And I can see list of last records
+#  And I can see first page button
+#    And next-button is invisible
+#
+#  Scenario: Correct navigation with arrow-button between the pages
+#    Given I go to ShopMe main page
+#    When I enter a searching phrase "test" into the search field
+#    And I click the search button
+#    And search results are visible
+#    And I click next-button
+#    Then I can see that number 2 is bold
+  # And I can see 10 results from the second page
+#    And previous-button is visible
+#    And I can return to the first page by clicking previous-button
+#
+#  Scenario: Add offer and check correctness pagination
+#    Given I go to ShopMe main page
+#    And I enter a searching phrase "test" into the search field
+#    And I click the search button
+#    And search results are visible
+#  When I click third page button
+#    And I have a list of saved services for phrase "test"
+#    And I add services
+#    # | service name | category | user name | user email      | user phone | user info | base description | base price | extended description | extended price | extra description | extra price | province          | city     |
+#      | special test | law      | test      | test@domain.com | 888555222  | test      | test             | 10         | test                 | 20             | test              | 30          | WesternPomeranian | Szczecin |
+#    And I click fourth page button
+#    Then The results for "test" are shifted one forward
+#
+#  Scenario: Delete offers and check correctness pagination
+#      Given I go to ShopMe main page
+#      And I enter a searching phrase "test" into the search field
+#      And I click the search button
+#      And search results are visible
+#      When I click third page button
+#      And I have a list of saved services for phrase "test"
+#      And I delete 10 services from the page number 2 with searching phrase "test"
+#      And I click fourth page button
+#      Then The results for "test" are shifted 10 to the back
--- src/test/java/tests/pages/SearchResultsPage.java
+++ src/test/java/tests/pages/SearchResultsPage.java
@@ -1,15 +1,21 @@
 package tests.pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import tests.objects.MyService;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
 
+import static org.junit.Assert.assertTrue;
 import static tests.Hooks.driver;
 import static tests.Hooks.wait;
 
@@ -30,9 +36,172 @@ public class SearchResultsPage extends SearchServicePage {
     @FindBy(how = How.CLASS_NAME, using = "services-item")
     public List<WebElement> ServiceList;
 
+    @FindBy(how = How.CLASS_NAME, using = "pagination__button--last")
+    public WebElement lastPageButton;
+
+    @FindBy(how = How.CLASS_NAME, using = "pagination__button--next")
+    public WebElement nextButton;
+
+    @FindBy(how = How.CLASS_NAME, using = "pagination__list")
+    public WebElement paginationList;
+
+    @FindBy(how = How.CLASS_NAME, using = "pagination__button")
+    public List<WebElement> pageButtonsList;
+
+    @FindBy(how = How.CLASS_NAME, using = "pagination__button--active")
+    public WebElement activePageButton;
+
+    @FindBy(how = How.CLASS_NAME, using = "pagination__button--previous")
+    public WebElement previousButton;
+
     private WebElement getServiceRowElement(int line) {
         return ServiceList.get(line);
     }
+
+    private WebElement getNumberOfPage(int pageNumber) {
+                return pageButtonsList.get(pageNumber);
+            }
+
+            public void pushPageNumberButton(int number, int page) {
+                getNumberOfPage(number).click();
+                wait.until(ExpectedConditions.textToBePresentInElement(activePageButton, String.valueOf(page)));
+            }
+
+            public void pushNextPageNumberButton(int number) {
+                if (number == 0)
+                        getNumberOfPage(number).click();
+                else try {
+                        if (getNumberOfPage(number + 1).getText().equals(String.valueOf(number + 2)))
+                                getNumberOfPage(number + 1).click();
+                        else
+                            getNumberOfPage(5).click();
+                    } catch (IndexOutOfBoundsException e) {
+                        getNumberOfPage(5).click();
+                    }
+            }
+
+            public void pushNextButton() {
+                nextButton.click();
+            }
+
+            public void returnToFirstPage() {
+                int lastPage = Integer.valueOf(activePageButton.getText());
+                int actualPage = lastPage;
+                wait.until(ExpectedConditions.textToBePresentInElement(pageButtonsList.get(0), "<"));
+                for (int i = 1; i <= lastPage; i++) {
+                        pageButtonsList.get(0).click();
+                        wait.until(ExpectedConditions.textToBePresentInElement(activePageButton, String.valueOf(actualPage)));
+                        --actualPage;
+                    }
+                assertTrue(activePageButton.getText().equals("1"));
+            }
+
+            public void areLastServicesVisible() {
+                wait.until(ExpectedConditions.visibilityOf(previousButton));
+                int lastPage = Integer.valueOf(activePageButton.getText());
+                if (lastPage < 10) {
+                        assertTrue(getServicesTitles().get(0).substring(0, 2).equals(String.valueOf((lastPage - 1) * 10 + 1)));
+                    } else {
+                        assertTrue(getServicesTitles().get(0).substring(0, 3).equals(String.valueOf((lastPage - 1) * 10 + 1)));
+                    }
+            }
+
+            public String getLastPage() {
+                wait.until(ExpectedConditions.visibilityOf(lastPageButton));
+                return lastPageButton.getText();
+            }
+
+            public Integer getNumberOfPagesFromFE() {
+                Integer pageNumberInFE;
+                if (pageButtonsList.size() <= 3) {
+                        pageNumberInFE = pageButtonsList.size();
+                    } else {
+                        pageNumberInFE = Integer.valueOf(getLastPage());
+                    }
+                return pageNumberInFE;
+            }
+
+            public void isCorrectNumberOfResultsOnEachPage(int numberOfResults) {
+                wait.until(ExpectedConditions.visibilityOf(nextButton));
+                int k = getNumberOfPagesFromFE();
+                for (int i = 0; i < k - 1; i++) {
+                        wait.until(ExpectedConditions.textToBePresentInElement(activePageButton, String.valueOf(i + 1)));
+                        isNumberOfServicesCorrect(numberOfResults);
+                        pushNextPageNumberButton(i);
+                    }
+                wait.until(ExpectedConditions.textToBePresentInElement(activePageButton, String.valueOf(k - 1)));
+            }
+
+            public void areAllDatabaseServicesPresent(String titlePhrase) {
+                wait.until(ExpectedConditions.visibilityOf(resultsList));
+                Integer pageNumberInFE = getNumberOfPagesFromFE();
+
+                        List<WebElement> elementsPerPage = resultsList.findElements(By.className("services-item__title"));
+                int last = elementsPerPage.size();
+                for (int j = 0; j < pageNumberInFE; j++) {
+                        for (int i = 0; i < last; i++) {
+                                MyService service = restAssuredMethods.searchForServices(titlePhrase).get(10 * j + i);
+                                //TITLE:
+                                        assertTrue(String.valueOf((10 * j + i + 1) + ". " + service.title).equals(getServicesTitles().get(i)));
+                                //PRICE:
+                                        int a = String.valueOf(getServicesPrices().get(i)).length();
+                                assertTrue((Float.valueOf(service.basePrice)).equals(Float.valueOf(getServicesPrices().get(i).substring(0, a - 2))));
+                                //DATE:
+                                        Date date = new Date(service.date);
+                                SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy");
+                                String dateText = df.format(date);
+                                String dateFE;
+                                if (getServicesDates().get(i).length() == 9) {
+                                        dateFE = "0" + getServicesDates().get(i);
+                                    } else dateFE = getServicesDates().get(i);
+                                assertTrue(dateText.equals(dateFE));
+                            }
+                        if (j + 1 < pageNumberInFE) {
+                                pushNextPageNumberButton(j);
+                                wait.until(ExpectedConditions.textToBePresentInElement(activePageButton, String.valueOf(j + 2)));
+                                elementsPerPage = resultsList.findElements(By.className("services-item__title"));
+                                last = elementsPerPage.size();
+                            }
+                    }
+            }
+
+            public void isNumberOfServicesCorrect(int numberPerPage) {
+                List<WebElement> titlesPerPage = resultsList.findElements(By.className("services-item__title"));
+                Assert.assertEquals(numberPerPage, titlesPerPage.size());
+            }
+
+            public void isNumberOfLastServicesUnderLimit(int limit) {
+                List<WebElement> titlesPerPage = resultsList.findElements(By.className("services-item__title"));
+                assertTrue(titlesPerPage.size() <= limit);
+            }
+
+
+            public void isNumberOfAllPagesVisible() {
+                if (pageButtonsList.size() == 0)
+                        assertTrue(getNumberOfPage(0).isDisplayed());
+                else if (pageButtonsList.size() == 1)
+                        assertTrue(getNumberOfPage(1).isDisplayed());
+                else if (pageButtonsList.size() == 2)
+                        assertTrue(getNumberOfPage(2).isDisplayed());
+                else if (pageButtonsList.size() == 3)
+                        assertTrue(getNumberOfPage(3).isDisplayed());
+                else
+                    assertTrue(lastPageButton.isDisplayed());
+            }
+
+            public void deleteServicesFromPage(int numberOfElements, int pageNumber, String title) {
+                List<MyService> deleteList = restAssuredMethods.searchForServicesOnPage(pageNumber, 10,title).content;
+                for (int i = 0; i < numberOfElements; ++i) {
+                        restAssuredMethods.deleteService(deleteList.get(i).id);
+                    }
+            }
+
+            public void pushLastPageButton() {
+                if (pageButtonsList.size() <= 3)
+                        pushPageNumberButton(pageButtonsList.size() - 1, pageButtonsList.size());
+                else
+                    lastPageButton.click();
+            }
 
 
     public SearchResultsPage() {
@@ -46,10 +215,48 @@ public class SearchResultsPage extends SearchServicePage {
     public void areNewResultsPresent(String expectedService) {
         wait.until(ExpectedConditions.textToBePresentInElement(firstService, expectedService));
     }
+    public void paginationButtonsAreVisible() {
+                wait.until(ExpectedConditions.visibilityOf(paginationList));
+            }
+
+            public void isActualPageBold(int numberOfPage) {
+                wait.until(ExpectedConditions.textToBePresentInElement(activePageButton, String.valueOf(numberOfPage)));
+            }
+
+            public void previousButtonIsVisible() {
+                assertTrue(pageButtonsList.get(0).getText().equals("<"));
+            }
+
+            public void firstPageButtonIsVisible() {
+                String firstPage = "1";
+                if (!activePageButton.getText().equals("1")) {
+                        firstPage = pageButtonsList.get(1).getText();
+                    }
+                assertTrue(firstPage.equals("1"));
+            }
+
+            public boolean isNextButtonInvisible() {
+                try {
+                        nextButton.isDisplayed();
+                        return false;
+                    } catch (org.openqa.selenium.NoSuchElementException e) {
+                        return true;
+                    }
+            }
+
+            public boolean isFirstPageButtonClickable() {
+                try {
+                        wait.until(ExpectedConditions.elementToBeClickable(getNumberOfPage(1)));
+                        return true;
+                    } catch (NoSuchElementException e) {
+                        return false;
+                    }
+            }
+
 
     public List<String> getServicesTitles() {
         List<WebElement> titlesWebElements = resultsList.findElements(By.className("services-item__title"));
-        ArrayList<String> titles = new ArrayList<String>();
+        ArrayList<String> titles = new ArrayList<>();
         for (int i = 0; i < titlesWebElements.size(); i++) {
             titles.add(titlesWebElements.get(i).getText());
         }
@@ -58,7 +265,7 @@ public class SearchResultsPage extends SearchServicePage {
 
     public List<String> getServicesPrices() {
         List<WebElement> pricesWebElements = resultsList.findElements(By.className("services-item__price"));
-        ArrayList<String> prices = new ArrayList<String>();
+        ArrayList<String> prices = new ArrayList<>();
         for (int i = 0; i < pricesWebElements.size(); i++) {
             prices.add(pricesWebElements.get(i).getText());
         }
@@ -67,7 +274,7 @@ public class SearchResultsPage extends SearchServicePage {
 
     public List<String> getServicesDates() {
         List<WebElement> datesWebElements = resultsList.findElements(By.className("services-item__date"));
-        ArrayList<String> dates = new ArrayList<String>();
+        ArrayList<String> dates = new ArrayList<>();
         for (int i = 0; i < datesWebElements.size(); i++) {
             dates.add(datesWebElements.get(i).getText());
         }
