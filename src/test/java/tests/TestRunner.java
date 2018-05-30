12af357eff03cc62b63a2fc2310c13f094c0bc2b
UlaLib <ulibront@gmail.com>
2018-05-30 09:33:34 +0200

    typo fixed

--- src/test/java/tests/TestRunner.java
+++ src/test/java/tests/TestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "features", tags = {"@workingGoo"})
+@CucumberOptions(features = "features", tags = {"~@inProgress"})
 
 
 public class TestRunner {
